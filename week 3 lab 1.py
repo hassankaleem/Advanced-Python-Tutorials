6e069952f872de966e23cefcea6b951499f071c1
Hassan Kaleem <ranahassankaleem99@yahoo.com>
2020-10-31 01:43:22 +0000

    Add files via upload
    Use of while loop

---
+++ week 3 lab 1.py
@@ -0,0 +1,6 @@
+def w_loop():
+    while True:
+        num = int(input("Enter the number that should either be, 1,2,3,4"))
+        if(num==1 or num==2 or num ==3 or num ==4):
+            break
+            
