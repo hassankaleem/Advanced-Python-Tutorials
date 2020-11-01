5a294388c1b003f375e39ac25f425608af5f38bb
hassankaleem <ranahassankaleem99@yahoo.com>
2020-11-01 23:20:22 +0000

    Create week 3 lecture 16last exercise.py
    nested list

---
+++ Unit 3 Week 3/week 3 lecture 16last exercise.py
@@ -0,0 +1,11 @@
+my_list = [[1,2,3],[4,5,6],[7,8,9]]
+
+for i in range(len(my_list[0])):
+    print(my_list[i][0],my_list[i][1],my_list[i][2])
+
+
+
+##my_list = [[1,2,3],[4,5,6],[7,8,9]]
+##for j in range(3):
+##    for i in range(len(my_list[0])):
+##        print(my_list[i][j])
