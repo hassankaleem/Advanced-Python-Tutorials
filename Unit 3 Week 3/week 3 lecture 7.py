b0e41b9cacd8f6d8a41b2ab80f5f3cfd56117599
uasif001 <uzairasif421@gmail.com>
2020-11-01 01:29:21 +0000

    concatenation of string

--- Unit 3 Week 3/week 3 lecture 7.py
+++ Unit 3 Week 3/week 3 lecture 7.py
@@ -9,24 +9,11 @@ def my_fun(arg_1):
 
 print(my_fun("Calculate number of time a comes in this stringaaaaa"))
 
-
-
-
-
 ## String is imutable data type where we can not instert somethin in it
-
-##let try
-
+##let try , 
 
 var1 = "This is my world!!"
+##var1[0]="a"    # error str' object does not support item assignment
 
-print(var1[3])
-
-
-
-
-
-
-
-
-
+var1 = 'a'+var1[1:len(var1)] #concatination + string mutation by addading a in it. 
+print(var1)
