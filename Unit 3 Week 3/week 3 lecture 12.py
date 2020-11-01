f95bcddb3cc73ec4be1f558f36ae9862f320805d
hassankaleem <ranahassankaleem99@yahoo.com>
2020-11-01 13:08:44 +0000

    Create week 3 lecture 12.py
    function that take list as input and print that in backward sequence.

---
+++ Unit 3 Week 3/week 3 lecture 12.py
@@ -0,0 +1,15 @@
+# create a function that can take list as parameter and reverse the sequence of the list.
+# input this ['hassan','salman','faizan']
+# output this['hassan','salman','faizan']
+
+
+
+def my_fun(my_list):
+    counter = len(my_list)
+    print(counter)
+    for index in range(counter):
+        counter-=1
+        print(index,my_list[counter])
+        
+
+my_fun(['hassan','salman','faizan'])
