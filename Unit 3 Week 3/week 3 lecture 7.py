edbf5acbaa4d612e1f6aedcdbe8cb12d656e83d5
uasif001 <uzairasif421@gmail.com>
2020-11-01 01:19:49 +0000

    use of while loop instead of for loop

---
+++ Unit 3 Week 3/week 3 lecture 7.py
@@ -0,0 +1,32 @@
+#Calculate numebr of time some thing repeat in the string
+def my_fun(arg_1):
+    count=0
+    for value in arg_1:
+        if(value=="a"):
+            count+=1   
+    return count
+
+
+print(my_fun("Calculate number of time a comes in this stringaaaaa"))
+
+
+
+
+
+## String is imutable data type where we can not instert somethin in it
+
+##let try
+
+
+var1 = "This is my world!!"
+
+print(var1[3])
+
+
+
+
+
+
+
+
+
