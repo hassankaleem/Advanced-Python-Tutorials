b922c5eb141036a72ab356430423936d1e356cc9
hassankaleem <ranahassankaleem99@yahoo.com>
2020-11-01 21:03:57 +0000

    Update week 3 lecture 14.py
    list

--- Unit 3 Week 3/week 3 lecture 14.py
+++ Unit 3 Week 3/week 3 lecture 14.py
@@ -1,2 +1,14 @@
 def my_fun(my_list):
     new_list = [''] * len(my_list)
+    print(new_list)
+    for i in range(len(my_list)):
+        print(my_list[len(my_list)-1-i])
+        new_list[i] = my_list[len(my_list)-1-i]
+    return new_list
+
+
+
+print(my_fun(['hassan','salman','Klaeem','ahmad']))
+
+
+
