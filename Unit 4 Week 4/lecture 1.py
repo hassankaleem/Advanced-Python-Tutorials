d739feecf3f19a6b0a5c879b3262f049f22e0b41
hassankaleem <ranahassankaleem99@yahoo.com>
2020-11-06 16:37:54 +0000

    add , append , extend list

---
+++ Unit 4 Week 4/lecture 1.py
@@ -0,0 +1,11 @@
+a=[1,2,3]
+b=[4,5,6]
+##c=a+b contatinate two lists
+##print(c)
+##a.append(b) append the new list as an element at the end of current list
+##a.append(4)#append new number at the end of list
+a.extend(b) # ?? extend only takes one argument
+a.append(1) # ?? append only takes one argument 
+a.append(b) 
+a.extend([0])
+a.extend(b)
